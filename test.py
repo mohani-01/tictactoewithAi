89100003fdd3a746c5f27ac430dd5a43a3320f69
Nahom <nahomohani@gmail.com>
2024-01-18 22:22:48 +0300

    work on the six functions one left

--- test.py
+++ test.py
@@ -1,15 +1,39 @@
-from tictactoe import actions
+from tictactoe import actions, result, winner, terminal
 
 
+EMPTY = None
+board =  [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, "X", EMPTY]]
+
+board2 =  [["X", "O", "X"],
+            ["O", "X", "O"],
+            ["O", "X",  "X"]]
 
 def test_action():
 
-    EMPTY = None
-    board =  [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, "X", EMPTY]]
+    
     print(actions(board))
 
+def test_result():
+
+   print(result(board, (2,1)))
+    
 
 
-test_action()
+
+
+def test_winner():
+    winingboard =  [["O", "O", EMPTY],
+            [EMPTY, "O", EMPTY],
+            [EMPTY, "X", "O"]]
+
+    print(winner(board2))
+
+def test_terminal():
+    print(terminal(board))
+    print(terminal(board2))
+
+test_terminal()
+
+test_winner()
--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import copy
 
 X = "X"
 O = "O"
@@ -60,28 +61,69 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    i , j = action
+    
+
+    changeboard = copy(board)
+
+    if changeboard[i][j] != None:
+        raise ValueError("This play have already have a value") 
+     
+    changeboard[i][j] = player(changeboard)
+    return changeboard
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    wins = [[(0,0 ), (0, 1), (0, 2)], 
+            [(1,0 ), (1, 1), (1, 2)], 
+            [(2,0 ), (2, 1), (2, 2)], 
+            [(0,0 ), (1, 0), (2, 0)], 
+            [(0,1 ), (1, 1), (2, 1)], 
+            [(0,2 ), (1, 2), (2, 2)],
+            [(0,0 ), (1, 1), (2, 2)], 
+            [(0, 2), (1,1 ), (2, 0)]]
+        
+    for win in wins:
+        firstx, firsty = win[0]
+        secondx, secondy = win[1]
+        thirdx, thirdy = win[2]
+
+        if board[firstx][firsty] == board[secondx][secondy] == board[thirdx][thirdy] and board[thirdx][thirdy] != EMPTY:
+            return board[firstx][firsty]
+    else:
+        return None
+
+    # raise NotImplementedError
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board):
+        return True
+    for rows in board:
+        for column in rows:
+            if column != EMPTY:
+                return False
 
-
+    return True
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner = winner(board)
+    if  winner == "X":
+        return 1
+    elif winner == "O":
+        return -1
+    else:
+        return 0
+    # raise NotImplementedError
 
 
 def minimax(board):
